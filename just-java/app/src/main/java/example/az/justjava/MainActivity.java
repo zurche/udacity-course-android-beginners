d9ecbe561f8438e09944ae40ccfe4226c6954795
Alejandro Zürcher <alejandro.zurcher@gmail.com>
2016-05-09 20:51:39 -0300

    Adding variables and improved layout

--- just-java/app/src/main/java/example/az/justjava/MainActivity.java
+++ just-java/app/src/main/java/example/az/justjava/MainActivity.java
@@ -2,12 +2,40 @@ package example.az.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numberOfCoffees = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        displayQuantity(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int price) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
     }
 }
