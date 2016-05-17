eac7d02f203c0211d7be7db5e8cd54b38a733119
Alejandro Zürcher <alejandro.zurcher@gmail.com>
2016-05-16 21:05:46 -0300

    Completing Just Java app

--- just-java/app/src/main/java/example/az/justjava/MainActivity.java
+++ just-java/app/src/main/java/example/az/justjava/MainActivity.java
@@ -1,9 +1,11 @@
 package example.az.justjava;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -12,10 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     int price = 5;
     int quantity = 0;
-    private boolean hasAddedWhippedCream;
-    private boolean hasAddedChocolate;
     private CheckBox mWhippedCreamCheck;
     private CheckBox mChocolateCheck;
+    private EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +25,26 @@ public class MainActivity extends AppCompatActivity {
 
         mWhippedCreamCheck = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         mChocolateCheck = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        mNameEditText = (EditText) findViewById(R.id.name_edit_text);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        calculateOrderTotal();
+
         displayMessage(createOrderSummary(quantity * price));
+    }
+
+    private void calculateOrderTotal() {
+        if(mWhippedCreamCheck.isChecked()) {
+            price += 1;
+        }
+
+        if(mChocolateCheck.isChecked()) {
+            price += 2;
+        }
     }
 
     private void displayMessage(String message) {
@@ -42,8 +56,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void displayQuantity(int number) {
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
@@ -53,27 +66,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
-        quantity = 2;
-        quantity = quantity + 1;
-        displayQuantity(quantity);
+        if(quantity < 100){
+            quantity += 1;
+            displayQuantity(quantity);
+        }
     }
 
     public void decrement(View view) {
-        quantity = 2;
-        quantity = quantity - 1;
-        displayQuantity(quantity);
+        if(quantity > 0) {
+            quantity -= 1;
+            displayQuantity(quantity);
+        }
     }
 
     public String createOrderSummary(int orderPrice) {
-        hasAddedWhippedCream = mWhippedCreamCheck.isChecked();
-        hasAddedChocolate = mChocolateCheck.isChecked();
-
         String stringOrderPrice = NumberFormat.getCurrencyInstance().format(orderPrice);
-        return "Name: Kaptain Kunal" +
-                "\nAdd whipped cream? " + hasAddedWhippedCream +
-                "\nAdd chocolate? " + hasAddedChocolate +
+        String orderSummary = "Name: " + mNameEditText.getText().toString() +
+                "\nAdd whipped cream? " + mWhippedCreamCheck.isChecked() +
+                "\nAdd chocolate? " + mChocolateCheck.isChecked() +
                 "\nQuantity: " + quantity +
                 "\nTotal: " + stringOrderPrice +
                 "\nThank you!";
+        sendOrderToMail(orderSummary, mNameEditText.getText().toString());
+        return orderSummary;
+    }
+
+    private void sendOrderToMail(String orderSummary, String name){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        Intent mailer = Intent.createChooser(intent, null);
+        startActivity(mailer);
     }
 }
